cb1d1df6dd3ae008d833e3db941a56016bb55b2a
zhihen.Wang <wzcboss@qq.com>
2019-01-30 20:04:19 +0800

    init

---
+++ src/components/base.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="base">
+    <el-input type="textarea" :row="10" placeholder="input something..." v-model="textarea"></el-input>
+    <el-row></el-row>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      textarea: ""
+    };
+  }
+};
+</script>
+
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import Decoder from './views/Decoder.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -12,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/decoder',
+      name: 'decoder',
+      component:Decoder
     },
     {
       path: '/about',
